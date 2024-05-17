e90c638e96dde1951d67dbee65595597b40482a3
josna <josna.t@mobatia.com>
2024-05-17 15:20:03 +0530

    paly store release 1.6 by sajna : 17/05/2024

--- app/src/main/java/com/nas/naisak/constants/ApiClient.kt
+++ app/src/main/java/com/nas/naisak/constants/ApiClient.kt
@@ -7,8 +7,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    //   var BASE_URL = "https://core-naisak.nais.qa/"
-    var BASE_URL = " http://naisakv2.mobatia.in:8081/"
+    var BASE_URL = "https://core-naisak.nais.qa/"
+  //  var BASE_URL = " http://naisakv2.mobatia.in:8081/"
 
 
     val getClient: ApiInterface
